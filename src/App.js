4a6678bcf0ae30687249e944b4d0e061dd89b036
Parthiban024 <parthiban@objectways.com>
2022-11-26 02:38:16 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import styles from './components/Signup/styles.module.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Main from './components/Main';
+import Signup from "./components/Signup";
+import Login from './components/Login';
 
-function App() {
+function App() { 
+  const user = localStorage.getItem("token")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Routes>
+      {user &&<Route path="/" exact element={<Main/>}/>}
+      <Route path="/signup" exact element={<Signup/>}/>
+      <Route path="/login" exact element={<Login/>}/>
+      <Route path="/" exact element={<Navigate replace to="/login"/>}/>
+     </Routes>
     </div>
   );
 }
